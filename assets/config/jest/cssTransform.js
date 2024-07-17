47af0da504facdaf8376adee3e4fb41954cc0588
jxiao21 <newsap98@gmail.com>
2024-07-17 12:00:13 -0400

    made all changes from PR except createHistogram.js

--- assets/config/jest/cssTransform.js
+++ assets/config/jest/cssTransform.js
@@ -1,14 +1,20 @@
-'use strict'
+// 'use strict'
 
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
+const babelJest = require('babel-jest');
+
 module.exports = {
-  process () {
-    return 'module.exports = {};'
+  // process () {
+  //   return 'module.exports = {};'
+  process(src, filename, config, options) {
+    return babelJest.process(src, filename, config, options);
   },
-  getCacheKey () {
-    // The output is always the same.
-    return 'cssTransform'
-  }
-}
+  // getCacheKey () {
+  //   // The output is always the same.
+  //   return 'cssTransform'
+  // },
+  'type': 'module',
+  extensionsToTreatAsEsm: ['.js', '.jsx']
+};
